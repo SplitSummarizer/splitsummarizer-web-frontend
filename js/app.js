413d130c87922bff542ed5ca7ff55ce63b35e2cb
Abhik Pal <abhikpal2509@gmail.com>
2016-08-24 15:27:56 +0530

    Added Drive Folder to git.

---
+++ js/app.js
@@ -0,0 +1,30 @@
+function animation(){
+   $("#logo").hide().show("slow");
+   $(".nav li").hide().show("slow");
+   $("#congrats").hide();
+}
+$(document).ready(animation);
+
+$("#rating").click(function(){
+		$("#rating").hide("slow");
+	})
+
+
+//var message = $("input").click(function(){
+   // line1.line2.show();
+//} //+(wordCount/7.5).toFixed(1)+"<p>"+seconds.+"</p>");
+//document.getElementById('words').style.cssText = "font-size: 70px;";*/
+		//<div class="congrats">
+//var textBox1 = $("#text1").textareaCounter();
+///var textBox2 = $("#text2").textareaCounter();
+//var wordCount = (textbox1 - textbox2)
+//var congrats = "Congratulations!";
+//var followMessage = "Split just saved you "+(wordCount/7.5).toFixed(1)+" seconds.";
+//var line1 = document.write("<h1>"+congrats+ "</h1>");
+//var line2 = document.write("<p>"+followMessage+"</p>");
+
+//$("#submit").click(function(){
+//	$(".congrats").show();
+//})
+
+
